a4eeee7a082ac3cd6b94182b3d72cdf107845341
Flotss <manginf54@gmail.com>
2022-12-07 00:12:31 +0100

    Added execution in a main class

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
 # ADVENT OF CODE
 
-This is my attempt at the [Advent of Code](https://adventofcode.com/) puzzles. I'm using this as an opportunity to learn [java](https://www.java.com/en/).
+This is my attempt at the [Advent of Code](https://adventofcode.com/) puzzles. I'm using this as an opportunity to
+learn [java](https://www.java.com/en/).
 
-## Running the code
+## How to use it
 
-To run the code, you'll need to have [java](https://www.java.com/en/) installed. 
+To run the code, you'll need to have [java](https://www.java.com/en/) installed.
 
 Then, you have to compile the code. To do this, run the following command in the root directory of the project:
 
-    javac -d bin src/Day<day number>/*.java
+### Compiling the code
+
+Windows :
+
+    dir .\src\*.java /s /B > sources.txt
+    javac -d bin @sources.txt
+
+Linux :
+
+    find ./src -name "*.java" > sources.txt
+    javac -d bin @sources.txt
+
+### Running the code
 
 Then, you can run the code with the following command:
 
-    java -cp bin Day<number>.Part<number>
+    java -cp bin Run
+
+### Exemple
+
+<p align="center">
+    <img src="https://i.imgur.com/PY0cT4h.png" alt="exemple">
+</p>
+
+## IMPORTANT ! Inputs
+
+You must change your inputs in  :
+
+        src/inputs/
+
+Else, the result will be the result of [Flotss](https://github.com/Flotss) and not yours.
+
---
+++ src/Run.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Run {
+
+    private final Map<String, Class<?>> classes = new TreeMap<>();
+
+    public Run() throws ClassNotFoundException, InterruptedException {
+        ArrayList<File> nameClasses = new ArrayList<>();
+        File folder = new File("src");
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isDirectory()) {
+                for (File file2 : file.listFiles()) {
+                    String name = file2.getName();
+                    if (name.endsWith(".java")) {
+                        nameClasses.add(file2);
+                    }
+                }
+            }
+        }
+
+        System.out.println("Loading classes...");
+        for (File file : nameClasses) {
+            String name = file.getPath();
+            name = name.substring(0, name.length() - 5);
+            name = name.replace("\\", ".");
+            name = name.replace("src.", "");
+
+
+            System.out.print(name + " ");
+            if (name.endsWith("Part2")) {
+                System.out.println();
+            }
+            classes.put(name, Class.forName(name));
+            Thread.sleep(100);
+        }
+        System.out.println("Classes loaded.");
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Run run = new Run();
+
+        int numberOfDay = (run.classes.size() % 2 == 0) ? run.classes.size() / 2 : (run.classes.size() / 2) + 1;
+
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("Choose a day : 1 - " + numberOfDay);
+            int day;
+            do {
+                try {
+                    day = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println("Please enter a number");
+                    day = 0;
+                }
+                System.out.println("Choose a day : 1 - " + numberOfDay);
+            } while (day <= 0 || day > numberOfDay);
+
+            System.out.println("Choose a part : 1 or 2");
+            int part;
+            do {
+                try {
+                    part = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println("Please enter a number");
+                    part = 0;
+                }
+                System.out.println("Choose a part : 1 or 2");
+            } while (part != 1 && part != 2);
+
+            System.out.println("\nRunning day " + day + " part " + part + ", so the class is " + run.getClass("Day" + day + ".Part" + part));
+            Class<?> c = run.getClass("Day" + day + ".Part" + part);
+            Method m = run.getMethod(c, "main");
+            m.invoke(null, (Object) new String[0]);
+
+            System.out.println("\nDo you want to run another day ? (y/n)");
+            while (true) {
+                String answer = scanner.nextLine();
+                if (answer.equalsIgnoreCase("n")) {
+                    loop = false;
+                } else if (answer.equalsIgnoreCase("y")) {
+                    break;
+                } else {
+                    System.out.println("Please enter y or n");
+                }
+            }
+        }
+    }
+
+    public Class<?> getClass(String name) {
+        return classes.get(name);
+    }
+
+    public Method getMethod(Class<?> c, String name) {
+        Method[] methods = c.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equals(name)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+}

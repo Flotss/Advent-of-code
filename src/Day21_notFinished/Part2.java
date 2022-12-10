1b7afdb62060b8a43eee52fa7aa413068ef9f7b0
Flotss <manginf54@gmail.com>
2022-12-10 14:17:46 +0100

    Prepare files

---
+++ src/Day21_notFinished/Part2.java
@@ -0,0 +1,16 @@
+package Day21_notFinished;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Part2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("inputs/inputDay1.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+
+        }
+    }
+}
+
